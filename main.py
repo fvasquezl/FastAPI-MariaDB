645d5eade85e5bda774849770d3181412ca96ef6
Faustino Vasquez <vasquez.limon@gmail.com>
2024-04-02 15:46:48 -0700

    creating schemas

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from typing import Annotated
 import models
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
+from schemas.post import PostBase
+from schemas.user import UserBase
 
 
 app = FastAPI()
@@ -11,13 +13,13 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-class PostBase(BaseModel):
-    title:str
-    content:str
-    user_id:int
+# class PostBase(BaseModel):
+#     title:str
+#     content:str
+#     user_id:int
 
-class UserBase(BaseModel):
-    username:str
+# class UserBase(BaseModel):
+#     username:str
 
 
 def get_db():
@@ -27,24 +29,27 @@ def get_db():
     finally:
         db.close()
 
-    
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.post("/posts/",status_code=status.HTTP_201_CREATED)
-async def create_post(post:PostBase, db:db_dependency):
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: db_dependency):
     db_post = models.Post(**post.model_dump())
     db.add(db_post)
     db.commit()
 
-@app.get("/posts/{post_id}",status_code=status.HTTP_200_OK)
-async def read_post(post_id:int, db:db_dependency):
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def read_post(post_id: int, db: db_dependency):
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if post is None:
         raise HTTPException(status_code=404, detail="post not found")
     return post
 
-@app.delete("/posts/{post_id}",status_code=status.HTTP_200_OK)
-async def delete_post(post_id:int, db:db_dependency):
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post(post_id: int, db: db_dependency):
     db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if db_post is None:
         raise HTTPException(status_code=404, detail="post not found")
@@ -52,15 +57,15 @@ async def delete_post(post_id:int, db:db_dependency):
     db.commit()
 
 
-
-@app.post("/users/",status_code=status.HTTP_201_CREATED)
-async def create_user(user:UserBase, db:db_dependency):
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
     db_user = models.User(**user.model_dump())
     db.add(db_user)
     db.commit()
 
-@app.get("/users/{user_id}",status_code=status.HTTP_200_OK)
-async def read_user(user_id:int, db:db_dependency):
+
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
---
+++ schemas/post.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
